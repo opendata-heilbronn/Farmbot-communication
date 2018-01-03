0f17cc2a432140dc0a64cbe78b7f84632eaa2c60
Patrick <patrick2003>
2018-01-03 16:53:50 +0100

    {

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const SerialPort = require('serialport');
 const testcode = require('./testcode');
 const readline = require('readline');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const port = new SerialPort('/dev/ttyACM0', {
     baudRate: 115200
 });
 
+rl.on('line', (input) => {
+    if (input = 'testcode') {
+        sendCommand(testcode.movex);
+    }
+    else if (input[0] === 'F' || input[0] === 'G') {
+        sendCommand('\n' + input);
+    }
+    else console.log('"' + input + '"' + 'is not a valid command!')
+});
 
 function sendCommand(command) {
     port.write(command, function (err) {
@@ -22,6 +36,6 @@ port.on('error', function (err) {
 port.on("data", (data) => {
     console.log(data.toString());
 });
-setInterval(() => {
-    sendCommand(testcode.movex);
-}, 10000);
+
+
+setInterval(() => , 10000);
