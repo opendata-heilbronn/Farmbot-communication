7495ec198c4b9317d5860ce527bf7dd05603115b
Patrick <patrick2003>
2018-01-03 15:20:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const SerialPort = require('serialport');
+const testcode = require('./testcode');
+const readline = require('readline');
+
+const port = new SerialPort('/dev/ttyACM0', {
+    baudRate: 115200
+});
+
+
+function sendCommand(command) {
+    port.write(command, function (err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+        else console.log('message written');
+    });
+};
+
+port.on('error', function (err) {
+    console.log('Error: ', err.message);
+});
+port.on("data", (data) => {
+    console.log(data);
+});
+setInterval(() => {
+    sendCommand(testcode.movex);
+}, 10000);
